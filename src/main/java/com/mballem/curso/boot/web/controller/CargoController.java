0f988c5b383408659bd87b3fd83c537d8686ec19
rafael_franca <rafael_franca@ecglobal.com>
2019-05-17 17:31:23 -0300

    fixed

--- src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/CargoController.java
@@ -1,13 +1,31 @@
 package com.mballem.curso.boot.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mballem.curso.boot.domain.Cargo;
+import com.mballem.curso.boot.domain.Departamento;
+import com.mballem.curso.boot.service.CargoService;
+import com.mballem.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping("/cargos")
 public class CargoController {
 
+	
+	@Autowired
+	private CargoService cargoService;
+	
+	@Autowired
+	private DepartamentoService departamentoService;
+	
 	@GetMapping("/cadastrar")
 	public String cadastrar() {
 		return "/cargo/cadastro";
@@ -16,6 +34,18 @@ public class CargoController {
 	@GetMapping("/listar")
 	public String listar() {
 		return "/cargo/lista";
+	}
+	
+	@PostMapping
+	public String salvar(Cargo cargo, RedirectAttributes attr) {
+		cargoService.salvar(cargo);
+		attr.addFlashAttribute("success", "Cargo inserido com sucesso");
+		return "redirect:/cargos/cadastrar";
+	}
+	
+	@ModelAttribute("departaemetos")
+	public List<Departamento> listaDeDepartamentos() {
+		return departamentoService.buscarTodos();
 	}
 
 }
--- src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -31,36 +31,35 @@ public class DepartamentoController {
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(Departamento departamento) {
+	public String salvar(Departamento departamento, 	RedirectAttributes attr) {
 		service.salvar(departamento);
+		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 	}
-	
-	
+
 	@GetMapping("/editar/{id}")
 	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
 		model.addAttribute("departamento", service.buscarPorId(id));
 		return "/departamento/cadastro";
 	}
-	
+
 	@PostMapping("/editar")
 	public String editar(Departamento departamento, RedirectAttributes attr) {
 		service.editar(departamento);
 		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/departamentos/cadastrar";
 	}
-	
+
 	@GetMapping("/excluir/{id}")
 	public String excluir(@PathVariable("id") Long id, ModelMap model) {
-		service.excluir(id);
-		/*
-		 * if (!service.depertamentoTemCargos(id)) { model.addAttribute("fail",
-		 * "Departamento não removido. Possui cargo(s) vinculado(s)."); } else {
-		 * service.excluir(id); model.addAttribute("success",
-		 * "Departamento excluído com sucesso."); }
-		 */
-		
+		if (service.depertamentoTemCargos(id)) {
+			model.addAttribute("fail", "Departamento não removido. Possui cargo(s) vinculado(s).");
+		} else {
+			service.excluir(id);
+			model.addAttribute("success", "Departamento excluído com sucesso.");
+		}
+
 		return listar(model);
 	}
-	
+
 }
